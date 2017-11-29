c120e9ee8efa2dc068bd699ffd178bd9f80be576
wy15010242640@163.com <wy15010242640@163.com>
2017-11-29 19:49:12 +0800

    add noters

--- src/main/java/com/J_mongo/javaConnMongo.java
+++ src/main/java/com/J_mongo/javaConnMongo.java
@@ -11,6 +11,10 @@ import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import java.text.ParseException;
 
+/**
+ * java连接mongo，并写入数据
+ * created by yu.wang.a
+ */
 public class javaConnMongo{
     public static void main( String args[] ){
         try{
@@ -21,31 +25,31 @@ public class javaConnMongo{
             MongoDatabase mongoDatabase = mongoClient.getDatabase("mldn");
             System.out.println("Connect to database successfully");
 
-                MongoCollection<Document> collection = mongoDatabase.getCollection("student_accuracy");
-                System.out.println("集合 student_accuracy 选择成功");
-                /**
-                 * 1. 创建文档 org.bson.Document 参数为key-value的格式
-                 * 2. 创建文档集合List<Document>
-                 * 3. 将文档集合插入数据库集合中 mongoCollection.insertMany(List<Document>) 插入单个文档可以用
-                 *    mongoCollection.insertOne(Document)
-                 * */
-                String sub_set[]={"ENGLISH","MATH","CHINESE"};
-                Random random=new Random();
-                Date date = new Date();
-                DateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
-                String createtime = format.format(date);
-                List<Document> documents = new ArrayList<Document>();
-                for (int i=0;i < 1000000;i++) {
-                    int index=random.nextInt(sub_set.length);
-                    Document document = new Document("student_id",(int)(Math.random()*1000000000)).
-                            append("subject", sub_set[index]).
-                            append("create_at",format.parse(createtime)).
-                            append("accuracy",((int)(Math.random()*1000))/1000.0);
-                //isodate时间比本地时间早8hour
-                    documents.add(document);
-                }
-                //插入文档
-                collection.insertMany(documents);
+             MongoCollection<Document> collection = mongoDatabase.getCollection("student_accuracy");
+             System.out.println("集合 student_accuracy 选择成功");
+             /**
+              * 1. 创建文档 org.bson.Document 参数为key-value的格式
+              * 2. 创建文档集合List<Document>
+              * 3. 将文档集合插入数据库集合中 mongoCollection.insertMany(List<Document>) 插入单个文档可以用
+              *    mongoCollection.insertOne(Document)
+              * */
+             String sub_set[]={"ENGLISH","MATH","CHINESE"};
+             Random random=new Random();
+             Date date = new Date();
+             DateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
+             String createtime = format.format(date);
+             List<Document> documents = new ArrayList<Document>();
+             for (int i=0;i < 1000000;i++) {
+                 int index=random.nextInt(sub_set.length);
+                 Document document = new Document("student_id",(int)(Math.random()*1000000000)).
+                         append("subject", sub_set[index]).
+                         append("create_at",format.parse(createtime)).
+                         append("accuracy",((int)(Math.random()*1000))/1000.0);
+             //isodate时间比本地时间早8hour
+                 documents.add(document);
+             }
+             //插入文档
+             collection.insertMany(documents);
 
         }catch(Exception e){
             System.err.println( e.getClass().getName() + ": " + e.getMessage() );
